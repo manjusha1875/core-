6f727a4adf8edf07e22f959ae77a22248f67f76d
manjusha1875 <manjusha@gmail.com>
2026-09-10 12:57:46 +0530

    employee

---
+++ src/com/javafirstclass/Test4.java
@@ -0,0 +1,17 @@
+package com.javafirstclass;  //--private:-accessible only within the same class.
+
+  class Test4 {
+  private int a = 10; 
+
+   void display() {
+	System.out.println(a);
+	}
+
+}
+   class Demo{
+	public static void main(String [] args) {
+		Test4 t = new Test4();
+		t.display();
+	
+}
+	}
--- src/com/javafirstclass/student.java
+++ src/com/javafirstclass/student.java
@@ -5,7 +5,7 @@ public class student {
  int age = 21;
  String college = "st.mary's college";
 	void display() {
-		System.out.println("name:"+name);
+		System.out.println("name :"+name);
 		System.out.println("age :"+age);
 		System.out.println("college :"+college);
 	}
