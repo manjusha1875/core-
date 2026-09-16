19e2e0b8aa79abb0ff071515bc72e89c66c5c242
manjusha1875 <manjusha@gmail.com>
2026-09-16 13:01:06 +0530

    literal

---
+++ src/com/javafirstclass/Student2.java
@@ -0,0 +1,12 @@
+package com.javafirstclass;
+
+public class Student2 {
+int studid=7714;
+String studname="manju";
+int studsalary=20000;
+	public static void main(String[] args) {
+		System.out.println("welcome");
+
+	}
+
+}
